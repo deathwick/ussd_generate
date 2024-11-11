9d6fe53c53a9c420632494b5fbd643d3f4a2a953
deathwick <odugbesanopeoluwa@gmail.com>
2024-11-11 11:06:45 +0100

    modified:   src/server.js

--- src/server.js
+++ src/server.js
@@ -24,8 +24,8 @@ const PORT = process.env.PORT || 6000;
 
 async function startServer() {
   try {
-    await sequelize.authenticate();
-    console.log('Database connection established');
+    // await sequelize.authenticate();
+    // console.log('Database connection established');
     
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
